8634bf7c2d46ad59d41994109262fe9797b342f4
Tyler <tylerpharand@gmail.com>
2021-03-10 23:46:46 -0500

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,58 @@
+from gpsoauth import perform_master_login, perform_oauth
+from uuid import getnode as getmac
+from dotenv import load_dotenv
+from token_dispenser import TokenDispenser
+import os
+import requests
+import grpc
+
+from v1_pb2 import GetHomeGraphRequest
+from v1_pb2_grpc import StructuresServiceStub
+
+from google import auth as google_auth
+from google.auth.transport import requests as google_auth_transport_requests
+from google.auth.transport import grpc as google_auth_transport_grpc
+
+load_dotenv()
+
+# Generate GRPC
+# python -m grpc_tools.protoc --proto_path=./grpc_src --python_out=./python_out --grpc_python_out=./grpc_python_out v1.proto
+
+def main():
+    # TODO: Automatically get IP Address
+    ip = '192.168.0.11'
+    username = os.environ.get('GOOGLE_USERNAME')
+    password = os.environ.get('GOOGLE_PASSWORD')
+
+    # Get Access Token
+    token_dispener = TokenDispenser(username, password)
+    token = token_dispener.dispense()
+
+    # Get Devices on Network
+    creds = grpc.access_token_call_credentials(token)
+    ssl = grpc.ssl_channel_credentials()
+    composite = grpc.composite_channel_credentials(ssl, creds)
+    channel = grpc.secure_channel('googlehomefoyer-pa.googleapis.com:443', composite)
+    service = StructuresServiceStub(channel)
+    grpc_res = service.GetHomeGraph(GetHomeGraphRequest())
+    devices = grpc_res.home.devices
+
+    # Filter Devices by Google Nest Mini
+    google_nest_minis = [device for device in devices if device.hardware.model == 'Google Nest Mini']
+    local_auth_token = google_nest_minis[0].local_auth_token
+
+    # Fetch Alarms and Timers
+    res = requests.get(
+        url='https://{ip}:8443/setup/assistant/alarms'.format(ip=ip),
+        verify=False,
+        headers={'cast-local-authorization-token': local_auth_token },
+    )
+
+    if (res.status_code != 200):
+        raise Exception('Unable to reach Google Nest Mini ({ip})'.format(ip=ip))
+
+    timers = res.json()['timer']
+
+    print(timers)
+
+main()
---
+++ token_dispenser.py
@@ -0,0 +1,57 @@
+from gpsoauth import perform_master_login, perform_oauth
+from uuid import getnode as getmac
+
+class TokenDispenser:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.master_token = None
+        self.access_token = None
+
+    @staticmethod
+    def _get_android_id():
+        mac_int = getmac()
+        android_id = TokenDispenser._create_mac_string(mac_int)
+        android_id = android_id.replace(':', '')
+
+        return android_id
+
+    @staticmethod
+    def _create_mac_string(num, splitter=':'):
+        mac = hex(num)[2:]
+        if mac[-1] == 'L':
+            mac = mac[:-1]
+        pad = max(12 - len(mac), 0)
+        mac = '0' * pad + mac
+        mac = splitter.join([mac[x:x + 2] for x in range(0, 12, 2)])
+        mac = mac.upper()
+
+        return mac
+
+    def _get_master_token(self, android_id):
+        res = perform_master_login(self.username, self.password, android_id)
+        if 'Token' not in res:
+            raise Exception('Unable to fetch master token')
+
+        return res['Token']
+
+
+    def _get_access_token(self, master_token, android_id):
+        res = perform_oauth(
+            self.username, master_token, android_id,
+            app='com.google.android.apps.chromecast.app',
+            service='oauth2:https://www.google.com/accounts/OAuthLogin',
+            client_sig='24bb24c05e47e0aefa68a58a766179d9b613a600'
+        )
+
+        if 'Auth' not in res:
+            raise Exception('Unable to fetch access token')
+
+        return res['Auth']
+
+    def dispense(self):
+        device_id = self._get_android_id()
+        master_token = self._get_master_token(device_id)
+        access_token = self._get_access_token(master_token, device_id)
+
+        return access_token
